f97943d044915afe9b572984aee74fb8e2ebc36b
Yannic Kalka <y.kalka@outlook.com>
2021-10-15 11:43:08 +0200

    use default patterns when .modprunerrc was not found

--- src/lib/load-ignore-patterns.ts
+++ src/lib/load-ignore-patterns.ts
@@ -1,11 +1,18 @@
+import { yellow } from 'chalk';
 import { readFile } from 'fs/promises';
+import { basename } from 'path';
+import { DEFAULT_GLOBS } from './constants';
 
 export async function loadIgnorePatterns(file: string): Promise<string[]> {
   let fileContent = await readFile(file, 'utf-8').catch(() => null);
 
   // load the default globs if no config was found
   if (!fileContent) {
-    throw Error(`Config file "${file}" not found.`);
+    // Show a warning
+    console.warn(`Found no "${basename(file)}" file in the current working directory. Using the default patterns now.`);
+    console.warn(yellow('The default patterns may change over time and it is recommended to provide your own patterns file. You can create a patterns file with "npx mod-pruner init"'));
+
+    fileContent = DEFAULT_GLOBS.trim();
   }
 
   return fileContent
